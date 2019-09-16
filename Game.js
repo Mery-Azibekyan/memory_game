fae32f5daced2bec4b473e47739e40476cc6f119
Mery-Azibekyan <55389848+Mery-Azibekyan@users.noreply.github.com>
2019-09-16 15:33:59 +0400

    Create Game.js

---
+++ Game.js
@@ -0,0 +1,12 @@
+class Game {
+
+	card1 = {};
+	card2 = {};
+	scoreOutput = ""; //player
+	scoreElement = document.getElementById("score"); //player
+	stepElement= document.getElementById("step");
+	containerElem = document.getElementById("container");
+	levelElem = document.getElementById("level");
+	level = 1;
+
+}  
